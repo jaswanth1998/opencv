a5b2bcfcb0951eaad5396ede8a710142167b6923
jaswanth tata <jaswanthtata@gmail.com>
2017-11-13 22:42:26 +0530

    image draw in matplotlib

--- Untitled.py
+++ Untitled.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 img = cv2.imread('berry.jpg',cv2.IMREAD_GRAYSCALE)
 #IMREAD_COLOR = -1
 #IMREAD_UNCHAGED = -1 
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.imshow('image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+plt.imshow(img,cmap='gray',interpolation='bicubic')
+plt.plot([50,100],[80,100],'c',linewidth=5)
+plt.show()
